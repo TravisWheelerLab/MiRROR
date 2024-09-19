6a6c30cb4fa48083dcf5fd1a6faa3ad2f1a014bb
George Glidden-Handgis <47768122+georgeglidden@users.noreply.github.com>
2024-09-19 07:47:24 -0700

    further optimization

--- search.py
+++ search.py
@@ -1,3 +1,41 @@
+def _search_overlap2(spectrum: list[float], tolerance: float, key: float):
+    # identify candidate pairs with gap ~= a mass in the alphabet
+    n = len(spectrum)
+    candidate_pairs = []
+    for i in range(n):
+        p1 = spectrum[i]
+        for j in range(i + 1, n):
+            p2 = spectrum[j]
+            p_gap = p2 - p1
+            if p_gap - tolerance > key:
+                break
+            elif p_gap + tolerance < key:
+                continue
+            else:
+                p_delta = abs(key - p_gap)
+                if p_delta < tolerance:
+                    candidate_pairs.append((p1,p2))
+    # among candidate pairs, identify overlapping and disjoint pairs
+    m = len(candidate_pairs)
+    pivots_overlap = []
+    for i in range(m):
+        p1, p2 = candidate_pairs[i]
+        for j in range(i + 1, m):
+            q1, q2 = candidate_pairs[j]            
+            if q1 > p2:
+                break
+            elif p1 < q1 < p2 < q2:
+                pivots_overlap.append((p1,q1,p2,q2))
+
+    return pivots_overlap
+
+def search_overlap2(spectrum: list[float], tolerance: float, alphabet: list[float]):
+    pivots_overlap = []
+    for key in alphabet:
+        p_o = _search_overlap2(spectrum, tolerance, key)
+        pivots_overlap.extend(p_o)
+    return pivots_overlap
+    
 def _search_overlap(spectrum: list[float], tolerance: float, key: float):
     pivots = []
     n = len(spectrum)
@@ -38,13 +76,13 @@ def _search(spectrum: list[float], tolerance: float, key: float):
     n = len(spectrum)
     candidate_pairs = []
     for i in range(n):
-        p0 = spectrum[i]
+        p1 = spectrum[i]
         for j in range(i + 1, n):
             p2 = spectrum[j]
-            gap = p2 - p0
-            δ = abs(key - gap)
-            if δ < tolerance:
-                candidate_pairs.append((p0,p2))
+            p_gap = p2 - p1
+            delta = abs(key - p_gap)
+            if delta < tolerance:
+                candidate_pairs.append((p1,p2))
     # among candidate pairs, identify overlapping and disjoint pairs
     m = len(candidate_pairs)
     pivots_overlap = []
