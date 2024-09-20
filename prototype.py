f518aaeaadcdbf6e34011fdb148c1bbfe2c3a7b2
George Glidden-Handgis <47768122+georgeglidden@users.noreply.github.com>
2024-09-20 15:00:18 -0700

    misc changes

--- prototype.py
+++ prototype.py
@@ -11,8 +11,6 @@ from util import *
 from search import search as gap_driven_search
 from agnostic_search import search as agnostic_search
 
-SEARCHMODE = "gap-driven"
-
 def load_fasta_records_as_str(path_to_fasta):
     fasta_records = load_fasta_records(path_to_fasta)
     return [str(record.seq) for record in fasta_records]
@@ -20,13 +18,11 @@ def load_fasta_records_as_str(path_to_fasta):
 def generate_spectrum_and_list_mz(seq):
     return list_mz(generate_default_fragment_spectrum(seq))
 
-def locate_pivot_point(spectrum, tolerance):
-    global SEARCHMODE
-    if SEARCHMODE == "gap-driven":
+def locate_pivot_point(spectrum, tolerance, searchmode):
+    if searchmode == "gap-driven":
         search_fn = gap_driven_search
-    elif SEARCHMODE == "gap-agnostic":
+    elif searchmode == "gap-agnostic":
         search_fn = agnostic_search
-    #print(SEARCHMODE)
     pivots_overlap = search_fn(spectrum, "overlap", AMINO_MASS_MONO, tolerance)
     if len(pivots_overlap) == 0:
         pivots_disjoint = search_fn(spectrum, "disjoint", AMINO_MASS_MONO, tolerance)
@@ -44,9 +40,8 @@ def check_pivot(pivot, peptide, spectrum):
     true_pivot_location = np.mean(true_pivot)
     return abs(true_pivot_location - pivot) < 0.01
 
-def run_validation_test(argv):
-    global SEARCHMODE
-    print(f"search mode: {SEARCHMODE}")
+def run_validation_test(argv, searchmode):
+    print(f"search mode: {searchmode}")
     path_fasta = argv[2]
     tolerance = float(argv[3]) if len(argv) == 4 else 2 * AVERAGE_MASS_DIFFERENCE
     pool = multiprocessing.Pool()
@@ -77,8 +72,8 @@ def run_validation_test(argv):
 
     print("⚙\tlocating pivot points...")
     init_t = time()
-    spectra_with_tolerances = list(zip(spectra, [tolerance] * num_spec))
-    pivots = pool.starmap(locate_pivot_point, add_tqdm(spectra_with_tolerances, description="processing"))
+    spectra_tolerances_searchmode = list(zip(spectra, [tolerance] * num_spec, [searchmode] * num_spec))
+    pivots = pool.starmap(locate_pivot_point, add_tqdm(spectra_tolerances_searchmode, description="processing"))
     pivots_peptides_spectra = list(zip(pivots, tryptic_peptides, spectra))
     validation = pool.starmap(check_pivot, add_tqdm(pivots_peptides_spectra, description="validating"))
     num_successes = sum(validation)
@@ -89,27 +84,27 @@ def run_validation_test(argv):
     if num_successes != num_spec:
         print("⚙\trecording missed peptides...")
         init_t = time()
+        # get missed peptides
         unsolved_peptides = [x[1] for x in zip(validation,tryptic_peptides) if not(x[0])]
         num_misses = len(unsolved_peptides)
         assert num_misses == num_spec - num_successes 
+        # record
         input_fasta_suffix = os.path.basename(path_fasta)
         misses_dir = "./misses/"
         if not(os.path.exists(misses_dir)):
             os.mkdir(misses_dir)
-        misses_fasta = f"{misses_dir}misses_{SEARCHMODE}_{input_fasta_suffix}"
+        misses_fasta = f"{misses_dir}misses_{searchmode}_{input_fasta_suffix}"
         num_written = save_strings_to_fasta(misses_fasta, unsolved_peptides)
         assert num_written == num_misses
         elapsed_t = time() - init_t
         print(f"wrote {num_written} peptides to '{misses_fasta}' in {elapsed_t} seconds.")
+
 def main(argv):
-    global SEARCHMODE
     mode = argv[1]
     if mode == "test":
-        SEARCHMODE = "gap-driven"
-        run_validation_test(argv)
+        run_validation_test(argv, "gap-driven")
     elif mode == "test-agnostic":
-        SEARCHMODE = "gap-agnostic"
-        run_validation_test(argv)
+        run_validation_test(argv, "gap-agnostic")
     else:
         # todo: load spectrum from .mzML and .mzMLb
         print(f"unsupported mode `{mode}`")
