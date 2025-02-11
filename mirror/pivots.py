f2395b815a474c54f367a3ea01340dc95bc5d8c2
George Glidden-Handgis <47768122+georgeglidden@users.noreply.github.com>
2025-02-11 14:00:30 -0700

    removed subscript numerals b/c python's help() can't render them ):<

--- mirror/pivots.py
+++ mirror/pivots.py
@@ -94,9 +94,9 @@ def find_disjoint_pivots(
     gap_indices,
     tolerance,
 ):
-    """Find pairs of indexes (i₁,i₂), (j₁,j₂) into a list of peaks such that 
-    1. i₁ < i₂ < j₁ < j₂, that is, the intervals [i₁, i₂] and [j₁, j₂] are disjoint.
-    2. the difference  between spectrum[i₂] - spectrum[i₁] and spectrum[j₂] - spectrum[j₁] is less than `tolerance`.
+    """Find pairs of indexes (i1,i2), (j1,j2) into a list of peaks such that 
+    1. i1 < i2 < j1 < j2, that is, the intervals [i1, i2] and [j1, j2] are disjoint.
+    2. the difference  between spectrum[i2] - spectrum[i1] and spectrum[j2] - spectrum[j1] is less than `tolerance`.
     
     :spectrum: a sorted array of floats (peak mz values).
     :gap_indices: a list of integer 2-tuples which index into `spectrum`.
@@ -125,9 +125,9 @@ def find_overlapping_pivots(
     gap_indices,
     tolerance,
 ):
-    """Find pairs of indexes (i₁,i₂), (j₁,j₂) into a list of peaks such that 
-    1. i₁ < j₁ < i₂ < j₂, that is, the intervals [i₁, i₂] and [j₁, j₂] overlap.
-    2. the difference  between spectrum[i₂] - spectrum[i₁] and spectrum[j₂] - spectrum[j₁] is less than `tolerance`.
+    """Find pairs of indexes (i1,i2), (j1,j2) into a list of peaks such that 
+    1. i1 < j1 < i2 < j2, that is, the intervals [i1, i2] and [j1, j2] overlap.
+    2. the difference  between spectrum[i2] - spectrum[i1] and spectrum[j2] - spectrum[j1] is less than `tolerance`.
     
     :spectrum: a sorted array of floats (peak mz values).
     :gap_indices: a list of integer 2-tuples which index into `spectrum`.
@@ -152,10 +152,10 @@ def find_adjacent_pivots(
     spectrum,
     tolerance,
 ):
-    """Find pairs of indexes (i₁,i₂), (j₁,j₂) into a list of peaks such that 
-    1. i₁ < i₂ < j₁ < j₂, that is, the intervals [i₁, i₂] and [j₁, j₂] are disjoint.
-    2. (i₁, i₂, j₁, j₂) = (i₁, i₁ + 1, i₁ + 2, i₁ + 3), that is, the indices are all adjacent.
-    3. the difference  between spectrum[i₂] - spectrum[i₁] and spectrum[j₂] - spectrum[j₁] is less than `tolerance`.
+    """Find pairs of indexes (i1,i2), (j1,j2) into a list of peaks such that 
+    1. i1 < i2 < j1 < j2, that is, the intervals [i1, i2] and [j1, j2] are disjoint.
+    2. (i1, i2, j1, j2) = (i1, i1 + 1, i1 + 2, i1 + 3), that is, the indices are all adjacent.
+    3. the difference  between spectrum[i2] - spectrum[i1] and spectrum[j2] - spectrum[j1] is less than `tolerance`.
     
     :spectrum: a sorted array of floats (peak mz values).
     :tolerance: float, the threshold difference for equating two gaps."""
@@ -201,11 +201,11 @@ def construct_all_pivots(
     gap_results: list[GapResult],
     tolerance: float,
 ):
-    """Find pairs of indexes (i₁,i₂), (j₁,j₂) into a list of peaks such that 
-    1. the difference  between spectrum[i₂] - spectrum[i₁] and spectrum[j₂] - spectrum[j₁] is less than `tolerance`.
+    """Find pairs of indexes (i1,i2), (j1,j2) into a list of peaks such that 
+    1. the difference  between spectrum[i2] - spectrum[i1] and spectrum[j2] - spectrum[j1] is less than `tolerance`.
     2. the pivots are either (a) overlapping or (b) disjoint:
-        2a. i₁ < j₁ < i₂ < j₂, that is, the intervals [i₁, i₂] and [j₁, j₂] overlap.
-        2b. (i₁, i₂, j₁, j₂) = (i₁, i₁ + 1, i₁ + 2, i₁ + 3), that is, the indices are all adjacent.
+        2a. i1 < j1 < i2 < j2, that is, the intervals [i1, i2] and [j1, j2] overlap.
+        2b. (i1, i2, j1, j2) = (i1, i1 + 1, i1 + 2, i1 + 3), that is, the indices are all adjacent.
     
     :spectrum: a sorted array of floats (peak mz values).
     :gap_indices: a list of integer 2-tuples which index into `spectrum`.
@@ -222,11 +222,11 @@ def find_all_pivots(
     gap_results: list[GapResult],
     tolerance: float,
 ):
-    """Find pairs of indexes (i₁,i₂), (j₁,j₂) into a list of peaks such that 
-    1. the difference  between spectrum[i₂] - spectrum[i₁] and spectrum[j₂] - spectrum[j₁] is less than `tolerance`.
+    """Find pairs of indexes (i1,i2), (j1,j2) into a list of peaks such that 
+    1. the difference  between spectrum[i2] - spectrum[i1] and spectrum[j2] - spectrum[j1] is less than `tolerance`.
     2. the pivots are either (a) overlapping or (b) disjoint:
-        2a. i₁ < j₁ < i₂ < j₂, that is, the intervals [i₁, i₂] and [j₁, j₂] overlap.
-        2b. (i₁, i₂, j₁, j₂) = (i₁, i₁ + 1, i₁ + 2, i₁ + 3), that is, the indices are all adjacent.
+        2a. i1 < j1 < i2 < j2, that is, the intervals [i1, i2] and [j1, j2] overlap.
+        2b. (i1, i2, j1, j2) = (i1, i1 + 1, i1 + 2, i1 + 3), that is, the indices are all adjacent.
     Then, filter the pivots to remove pivots whose centers do not induce sufficient mirror symmetry in the peak list.
     
     :spectrum: a sorted array of floats (peak mz values).
