a6514cfea41e63cc493a5e9587cab585a414cdea
George Glidden-Handgis <47768122+georgeglidden@users.noreply.github.com>
2025-06-11 11:43:20 -0700

    add NineSpeciesBenchmarkPeakList

--- mirror/spectra/types.py
+++ mirror/spectra/types.py
@@ -1,3 +1,4 @@
+from abc import ABC, abstractclassmethod
 from typing import Any, Iterable
 from dataclasses import dataclass
 import warnings
@@ -100,7 +101,7 @@ class PeakList:
         return self.mz.__iter__()
     
     def get_intensity(self, i: int) -> float:
-        """Return the intensity of the `i`th peak."""
+        """The intensity of the `i`th peak."""
         return self.intensity[i]
 
 class AnnotatedPeakList(PeakList):
@@ -127,31 +128,33 @@ class AnnotatedPeakList(PeakList):
         return f"AnnotatedPeakList:\nmz: {self.mz}\nintensity: {self.intensity}\ncharge: {self.charge}\nlosses: {self.losses}\nmetadata:\n{metadata_repr}"
 
     def get_charge(self, i: int) -> int:
-        """The charge state of the `i`th peak."""
+        """Charge annotations of the `i`th peak."""
         return self.charge[i]
     
     def get_losses(self, i: int) -> int:
-        """The loss(es) of the `i`th peak."""
+        """Loss annotations of the `i`th peak."""
         return self.losses[i]
 
     def get_metadata_keys(self):
         return self.metadata.keys()
             
     def get_metadata(self, i: int, key: str) -> Any:
-        """Return the metadata for `key` of the `i`th peak."""
+        """Metadata for `key` of the `i`th peak."""
         return self.metadata[key][i]
-    
-    def compare_peak_annotations(self, other, i: int, keys):
+
+    def get_states(self, i: int, metadata_keys = []) -> set:
+        """The set of annotation vector(s) for the `i`th peak, including metadata if the `metadata_keys` kwarg is passed."""
+        charges = self.get_charge(i)
+        losses = self.get_losses(i)
+        metadata = [self.get_metadata(i, k) for k in metadata_keys]
+        states = [charges, losses, *metadata]
+        return set(zip(*states))
+
+    def compare_peak_annotations(self, other, i: int, metadata_keys):
         # collate self
-        s_charges = self.get_charge(i)
-        s_losses = self.get_losses(i)
-        s_meta = [self.get_metadata(i, k) for k in keys]
-        s_data = set(zip([s_charges, s_loses, *s_meta]))
+        s_data = set(self.get_states(i, metadata_keys))
         # collate other
-        o_charges = other.get_charge(i)
-        o_losses = other.get_losses(i)
-        o_meta = [other.get_metadata(i, k) for k in keys]
-        o_data = set(zip([o_charges, o_losses, *o_meta]))
+        o_data = set(other.get_states(i, metadata_keys))
         # compare
         matches = s_data.intersection(o_data)
         misses = s_data.symmetric_difference(o_data)
@@ -164,12 +167,12 @@ class AnnotatedPeakList(PeakList):
             raise ValueError("Mismatched m/z values; cannot directly compare annotations for distinct peak lists.")
         common_keys = set(self.get_metadata_keys()).intersection(other.get_metadata_keys())
         matches, misses = zip(*map(
-            lambda i: self.compare_peak_annotations(self, other, i, common_keys), 
+            lambda i: self.compare_peak_annotations(other, i, common_keys), 
             range(len(self))))
         return matches, misses
 
 class BenchmarkPeakList(AnnotatedPeakList):
-    """An annotated peak list in the format of the 9-species benchmark.
+    """An annotated peak list with a target peptide.
     Constructed via from_mzlib, and from_mgf class methods 
     or the simulate_peaks_from_peptide function of spectra.simulation."""
 
@@ -211,18 +214,18 @@ class BenchmarkPeakList(AnnotatedPeakList):
             def parse_annotation(annotation):
                 if type(annotation) == PeptideFragmentIonAnnotation:
                     return (
-                        [annotation.charge], 
-                        [annotation.mass_error], 
+                        [f"+{annotation.charge}"], 
+                        [annotation.mass_error.mass_error], 
                         [annotation.series], 
                         [annotation.position], 
-                        [annotation.neutral_losses])
+                        [tuple([''] if len(annotation.neutral_losses) == 0 else [loss.name for loss in annotation.neutral_losses])])
                 else:
                     return (
-                        None,
-                        None,
-                        None,
-                        None,
-                        None)
+                        ['+1'],
+                        [0.],
+                        [''],
+                        [-1],
+                        [tuple([''])])
             charge, mass_error, series, position, losses = zip(*map(
                 lambda x: parse_annotation(x[0]),
                 annotations))
@@ -232,9 +235,9 @@ class BenchmarkPeakList(AnnotatedPeakList):
                 mz = list(mz), 
                 intensity = list(intensity),
                 charge = list(charge),
-                losses = [None if L is None else [[loss.name for loss in l] for l in L] for L in losses],
+                losses = losses,
                 metadata = {
-                    "mass_error": [None if E is None else [e.mass_error for e in E] for E in mass_error],
+                    "mass_error": mass_error,
                     "series": list(series),
                     "position": list(position)})
 
@@ -255,7 +258,7 @@ class BenchmarkPeakList(AnnotatedPeakList):
         intensity = spectrum['intensity array']
         # peak annotations
         charge = [[c] for c in spectrum['charge array']]
-        losses = [[[None]] for _ in range(n)]
+        losses = [[['']] for _ in range(n)]
         # done
         return cls(
             peptide = seq,
@@ -264,3 +267,99 @@ class BenchmarkPeakList(AnnotatedPeakList):
             charge = charge,
             losses = losses,
             metadata = {})
+
+class NineSpeciesBenchmarkPeakList(BenchmarkPeakList):
+    """A benchmark peak list with peptide as well as 
+    series and position annotations on peaks. Can be 
+    constructed from the mzlib files of the nine-
+    -species benchmark."""
+
+    def __init__(self,
+        peptide: str,
+        mz: np.ndarray, 
+        intensity: np.ndarray,
+        charge: list,
+        losses: list,
+        series: list,
+        position: list,
+        *args,
+        **kwargs,
+    ):
+        self.peptide = peptide
+        self.series = series
+        self.position = position
+        super(BenchmarkPeakList, self).__init__(mz, intensity, charge, losses, *args, **kwargs)
+    
+    def get_series(self, i: int):
+        return self.series[i]
+    
+    def get_position(self, i: int):
+        return self.position[i]
+
+    def get_states(self, i: int, metadata_keys = []) -> set:
+        """The set of annotation vector(s) for the `i`th peak, including metadata if the `metadata_keys` kwarg is passed."""
+        charge = self.get_charge(i)[0]
+        losses = self.get_losses(i)[0]
+        series = self.get_series(i)[0]
+        position = self.get_position(i)[0]
+        metadata = [self.get_metadata(i, k) for k in metadata_keys]
+        return set([(charge, losses, series, position, *metadata)])
+    
+    def get_series_peaks(self, series: str):
+        for i in range(len(self)):
+            if self.get_series(i)[0] == series:
+                yield i
+    
+    def get_b_series_peaks(self):
+        return list(self.get_series_peaks('b'))
+    
+    def get_y_series_peaks(self):
+        return list(self.get_series_peaks('y'))
+
+    @classmethod
+    def from_mzlib(cls, 
+        dataset: mzlib.SpectrumLibrary,
+        i: int,
+    ):
+        """Given a spectrum library `dataset` and an index `i`,
+        retrieve the `i`th spectrum and use it to construct a BenchmarkPeakList.
+        The resulting object will have metadata for 'mass_error'."""
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            spectrum = dataset[i]
+            # peptide sequence
+            analyte = list(spectrum.analytes.values())[0]
+            seq = ''.join(map(
+                lambda x: x[0],
+                analyte.peptide.sequence))
+            # peak data
+            mz, intensity, annotations, _ = zip(*spectrum.peak_list)
+            # peak annotations
+            def parse_annotation(annotation):
+                if type(annotation) == PeptideFragmentIonAnnotation:
+                    return (
+                        [f"+{annotation.charge}"], 
+                        [annotation.mass_error.mass_error], 
+                        [annotation.series], 
+                        [annotation.position], 
+                        [tuple([''] if len(annotation.neutral_losses) == 0 else [loss.name for loss in annotation.neutral_losses])])
+                else:
+                    return (
+                        ['+1'],
+                        [0.],
+                        [''],
+                        [-1],
+                        [tuple([''])])
+            charge, mass_error, series, position, losses = zip(*map(
+                lambda x: parse_annotation(x[0]),
+                annotations))
+            # done
+            return cls(
+                peptide = seq,
+                mz = mz, 
+                intensity = intensity,
+                charge = list(charge),
+                losses = list(losses),
+                series = list(series),
+                position = list(position),
+                metadata = {"mass_error": mass_error})
